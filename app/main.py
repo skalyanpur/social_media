86790e8412aa2ffdcf46073d1780d10ef59b5dec
skalyanpur <sunil.kalyanpur@viasat.com>
2023-07-30 15:53:59 -0700

    wip crud operations

--- app/main.py
+++ app/main.py
@@ -15,8 +15,7 @@ app = FastAPI()
 class Post(BaseModel):
     title: str
     content: str
-    publish: bool = True
-    rating: Optional[int] = None
+    published: bool = True
 
 
 my_posts = []
@@ -29,25 +28,33 @@ async def root():
 
 @app.get("/posts")
 def get_posts(db: Session = Depends(get_db)):
-    return my_posts
+    return {"data": db.query(models.Post).all()}
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post, db: Session = Depends(get_db)):
-    my_posts.append(post)
-    return {"data": post}
+    new_post = models.Post(**post.model_dump())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return {"data": new_post}
 
 
 @app.get("/posts/{post_id}")
 def get_post(post_id: int, db: Session = Depends(get_db)):
-    return {"post_id": post_id}
+    post = db.query(models.Post).filter(models.Post.id == post_id).first()
+    return {"post_details": post}
 
 
 @app.put("/posts/{post_id}")
 def update_post(post_id: int, post: Post, db: Session = Depends(get_db)):
+
     return {"details": "Updated the post"}
 
 
 @app.delete("/posts/{post_id}")
 def delete_post(post_id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == post_id).first()
+    db.delete(post)
+    db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP, text
 
 from .database import Base
 
@@ -10,3 +10,4 @@ class Post(Base):
     title = Column(String, unique=True, index=True, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default="True", nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
